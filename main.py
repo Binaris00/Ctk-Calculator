c088b57720a17144d752f8ec96f7dea684e7636c
Binaris00 <129613904+Binaris00@users.noreply.github.com>
2023-05-02 22:05:32 -0300

    Completed main calculator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-
+import math
 
 # Basic Information for the calculator 
 root = ctk.CTk()
@@ -10,7 +10,7 @@ root.resizable(False, False)
 operation = ""
 
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+ctk.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 conditions = {"": [".", "+", "-", "/", "*"]}
@@ -35,18 +35,20 @@ def add_operation(symbol):
       if operation[-1] in conditions[""] and symbol in conditions[""]:
          return
    
+   if "(" not in operation and symbol == ")":
+      return
+   
    # Add the symbol to operation and output
    operation += str(symbol)
    output.delete(0.0, "end")
    output.insert(0.0, operation)
 
-
 def delete_all_operation():
+   """Just Delete the operation and output"""
    global operation
    
    operation = ""
    output.delete(0.0, "end")
-
 
 def delete_last_char():
    global operation
@@ -62,25 +64,72 @@ def eval_equal():
    try:
       operation = str(eval(operation))
    except:
+      if "/0" in operation:
+         operation = "You cannot divide by zero"
+      else:
+         operation = "Syntax Error"
+      
+   output.delete(0.0, "end")
+   output.insert(0.0, operation)
+   if operation == "Syntax Error":
+      operation = ""
+
+
+def operation_factorial():
+   global operation
+   if operation == "":
+      return
+   try:
+      operation = str(math.factorial(int(operation)))
+   except:
+      operation = "Syntax Error"
+      
+   
+   output.delete(0.0, "end")
+   output.insert(0.0, operation)
+   
+def power_two():
+   global operation
+   if operation == "":
+      return
+   try:
+      operation = str(math.pow(float(eval(operation)), 2))
+   except:
       operation = "Syntax Error"
       
    output.delete(0.0, "end")
    output.insert(0.0, operation)
-   operation = ""
+   
+def operation_sqrt():
+   global operation
+   if operation == "":
+      return
+   try:
+      operation = str(math.sqrt(eval(operation)))
+   except:
+      operation = "Syntax Error"
+   
+   output.delete(0.0, "end")
+   output.insert(0.0, operation)
+   
 
-output = ctk.CTkTextbox(root, width=490, height=50, font=(('Roboto', 70)), corner_radius=1)
+output = ctk.CTkTextbox(root, width=490, height=50, font=(('Roboto', 40)), corner_radius=1)
 output.grid(row=0, column=0, columnspan=5, padx=5, pady=5)
 
 """
 ------------
 (  )  CE C
-O  O  O  ÷
+x²  n!  O  ÷
 7  8  9  x
 4  5  6  -
-1  2  3  +
+1  2  3   +
 0  .  O  =
 """
-#######
+
+
+####### First Row 1
+
+
 
 button_parenthesis_1 = ctk.CTkButton(root, text="(", command=lambda: add_operation("("), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_parenthesis_1.grid(row=1, column=0, padx=1, pady=1)
@@ -94,21 +143,29 @@ button_del_all.grid(row=1, column=2, padx=1, pady=1)
 button_del = ctk.CTkButton(root, text="CE", command=lambda: delete_all_operation(), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_del.grid(row=1, column=3, padx=1, pady=1)
 
+
+
 #######
 
-button_empty = ctk.CTkButton(root, text=" ", command=print("Nothing :D"), width=120, height=80, font=("Roboto", 22), corner_radius=1)
+
+
+button_empty = ctk.CTkButton(root, text="x²", command=lambda: power_two(), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_empty.grid(row=2, column=0, padx=1, pady=1)
 
-button_empty = ctk.CTkButton(root, text=" ", command=print("Nothing :D"), width=120, height=80, font=("Roboto", 22), corner_radius=1)
+button_empty = ctk.CTkButton(root, text="n!", width=120, command=lambda: operation_factorial(),height=80, font=("Roboto", 22), corner_radius=1)
 button_empty.grid(row=2, column=1, padx=1, pady=1)
 
-button_empty = ctk.CTkButton(root, text=" ", command=print("Nothing :D"), width=120, height=80, font=("Roboto", 22), corner_radius=1)
+button_empty = ctk.CTkButton(root, text="√", command=lambda: operation_sqrt(),width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_empty.grid(row=2, column=2, padx=1, pady=1)
 
 button_division = ctk.CTkButton(root, text="÷", command=lambda: add_operation("/"), width=120, height=80,font=("Roboto", 22), corner_radius=1)
 button_division.grid(row=2, column=3, padx=1, pady=1)
 
+
+
 # BUTTONS NUMBERS
+
+
 
 button_7 = ctk.CTkButton(root, text="7", command=lambda: add_operation(7), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_7.grid(row=7, column=0, padx=1, pady=1)
@@ -122,7 +179,12 @@ button_9.grid(row=7, column=2, padx=1, pady=1)
 button_multiplication = ctk.CTkButton(root, text="x", command=lambda: add_operation("*"), width=120, height=80,font=("Roboto", 22), corner_radius=1)
 button_multiplication.grid(row=7, column=3, padx=1, pady=1)
 
+
+
+
 #######
+
+
 
 button_4 = ctk.CTkButton(root, text="4", command=lambda: add_operation(4), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_4.grid(row=8, column=0, padx=1, pady=1)
@@ -136,7 +198,11 @@ button_6.grid(row=8, column=2, padx=1, pady=1)
 button_subtraction = ctk.CTkButton(root, text="-", command=lambda: add_operation("-"), width=120, height=80,font=("Roboto", 22), corner_radius=1)
 button_subtraction.grid(row=8, column=3, padx=1, pady=1)
 
+
+
 #######
+
+
 
 button_1 = ctk.CTkButton(root, text="1", command=lambda: add_operation(1), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_1.grid(row=9, column=0, padx=1, pady=1)
@@ -151,18 +217,22 @@ button_addition = ctk.CTkButton(root, text="+", command=lambda: add_operation("+
 button_addition.grid(row=9, column=3, padx=1, pady=1)
 
 
+
 ####
 #Ultimate row
 button_0 = ctk.CTkButton(root, text="0", command=lambda: add_operation(0), width=120, height=80, font=("Roboto", 22), corner_radius=1)
 button_0.grid(row=10, column=0, padx=1, pady=1)
 
-button_point = ctk.CTkButton(root, text=".", command=lambda: add_operation("."), width=120, height=80, font=("Roboto", 22), corner_radius=1)
-button_point.grid(row=10, column=1, padx=1, pady=1)
+button_empty = ctk.CTkButton(root, text="00", command=lambda: add_operation("00"), width=120, height=80, font=("Roboto", 22), corner_radius=1)
+button_empty.grid(row=10, column=1, padx=1, pady=1)
 
-button_empty = ctk.CTkButton(root, text=" ", command=print("Nothing :D"), width=120, height=80, font=("Roboto", 22), corner_radius=1)
-button_empty.grid(row=10, column=2, padx=1, pady=1)
+button_point = ctk.CTkButton(root, text=".", command=lambda: add_operation("."), width=120, height=80, font=("Roboto", 22), corner_radius=1)
+button_point.grid(row=10, column=2, padx=1, pady=1)
 
 button_equal = ctk.CTkButton(root, text="=", command=lambda: eval_equal(), width=120, height=80 ,font=("Roboto", 22), corner_radius=1)
 button_equal.grid(row=10, column=3, padx=1, pady=1)
 
+
+
+##########
 root.mainloop()
